0040eaf06a9560933f00f9434d92670c7195ad05
Jeff McCune <jeff@puppetlabs.com>
2011-06-21 14:07:45 -0700

    (#8010) Add basic structure for group management
    This change adds a basic structure to the accounts module.  Data will live in
    the accounts::data namespace by default.  The user can easily customize where
    the module looks for data by setting the data_namespace class parameter.
    
    The module SHOULD look to a global parameter to support the Dashboard being
    used to set this location.
    
    The lookupvar() function is being added to the stdlib module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,18 @@
 # Class: accounts
 #
-# This module manages accounts
+#   This module manages accounts on a Puppet managed system.
+#
+#   FIXME: Add more information here
 #
 # Parameters:
+#
+#  [*data_namespace*] The Puppet namespace to find data.  Override the default
+#  of 'accounts::data' with your own namespace.  The string must end with
+#  ::data.  site::accounts::data is a good choice.
+#
+#  [*manage_groups*] Whether or not this module manages a set of default shared
+#  groups.  These groups must be defined in the $groups hash in the
+#  _data_namespace_
 #
 # Actions:
 #
@@ -10,8 +20,27 @@
 #
 # Sample Usage:
 #
-# [Remember: No empty lines between comments and class definition]
-class accounts {
+class accounts (
+  $data_namespace = 'accounts::data',
+  $manage_groups  = true
+) {
+
+  validate_re($data_namespace, '::data$')
+  $data_namespace_real = $data_namespace
+  validate_bool($manage_groups)
+  $manage_groups_real = $manage_groups
+
+  # This section of the code is repsonsible for pulling in the data we need.
+  $groups_hash = lookupvar("${data_namespace_real}::groups_hash")
+
+  debug("ACCOUNTS: groups_pash = $groups_hash")
+
+  anchor { "accounts::begin": }
+  anchor { "accounts::end": }
+
+  if $manage_groups_real {
+    class { 'accounts::groups': }
+  }
 
 
 }
