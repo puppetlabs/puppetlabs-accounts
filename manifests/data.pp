75c43e26bfda05aeabc76148d28205fbbbdae0b0
Jeff McCune <jeff@puppetlabs.com>
2011-06-21 17:23:34 -0700

    (#8010) Documentation only update to data module
    The data module is a reference for the end user to copy.  This change
    adds additional information to the documentation header of the class.

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,9 +1,9 @@
 # Class: accounts::data
 #
-#   This is the default set of data provided to with the accounts
-#   module.   Customers should define their own data class and configure
-#   the accounts module using the top level $param_accounts_data_namespace
-#   variable, or setting the class parameter when declaring the module.
+#   This is the default set of data provided to with the accounts module.
+#   Customers should define their own data class and configure the accounts
+#   module using the top level $param_accounts_data_namespace variable, or
+#   setting the class parameter when declaring the module.
 #
 # Parameters:
 #
@@ -12,6 +12,16 @@
 # Requires:
 #
 # Sample Usage:
+#
+# Make a copy of this class into your "site" module (Don't edit this class itself)
+# and reconfigure the accounts module to use your data rather than it's own by
+# specifying the namespace to lookup data in.
+#
+# node default {
+#   class { 'accounts':
+#     data_namespace => 'site::accounts::data',
+#   }
+# }
 #
 class accounts::data {
   $groups_hash = {
