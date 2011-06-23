07b071024d34b347626b1e86c532c2dc0858f1d1
Jeff McCune <jeff@puppetlabs.com>
2011-06-23 15:31:02 -0700

    (#8010) Add merging with the default hash
    This change adds support for the end user to specify default parameters for all
    of the user resources in a variable named $users_hash_default.
    
    Unlike the $users_hash, this is a single level parameter/value hash.
    
    When a parameter for the user resource exists in the default hash but not in
    the specific user hash, this change will merge in the value from the default
    hash.
    
    Filtering is done such that only parameters valid for the User resource type
    make it through the filter and into the create_resources() function.
    
    In addition, all parameter values are interpolated which may be a HORRIBLE idea
    to expose to the end user since we're calling eval() on user supplied data.
    
    Note, this interpolation make break down for values that are not strings.

--- manifests/users.rb
+++ manifests/users.rb
@@ -18,6 +18,9 @@ hostclass 'accounts::users' do
   # As a result, I'm relying on the fact the accounts class sets
   # $users_hash in it's scope before declaring this class.
 
+  # We need this list to filter out keys from the resource hashes we're dealing with.
+  valid_metaparams = %w{ require before subscribe notify tag }
+
   # This is how we bring Puppet DSL variables into a local variable in the Ruby DSL
   # (Validation of these variables should already be done in the accounts class)
   users_hash         = scope.lookupvar('::accounts::users_hash')
@@ -38,11 +41,27 @@ hostclass 'accounts::users' do
 
   # Now, add the group resources to the catalog:
   scope.function_create_resources(['group', gid_hash])
-  # And the user resources
-  scope.function_create_resources(['user', users_hash])
 
-  # Manage the home directory
-  users_hash.each do |title, param_hash|
+  # Filter the users_hash, rejecting anything not usable by the
+  # native user type.
+  # JJM Deliberately ignoring Solaris RBAC and Profile stuff
+  # JJM FIXME: Possibly add the account aging parameters?
+  valid_user_params = %w{ ensure home shell comment password uid
+    gid manage_home provider name managehome groups expiry } << valid_metaparams
+  users_rsrc_hash = users_hash.merge(users_hash) do |title, param_hash|
+    # merge in the defaults specified by the user
+    param_hash = users_hash_default.merge(param_hash)
+    # Strip out all 2nd level keys that aren't valid for the user type
+    param_hash.reject! { |param, value| !valid_user_params.include?(param) }
+    # FIXME Interpolate all string values (This may be a HORRIBLE idea)
+    # It may be better to ONLY interpolate the home directory or something
+    param_hash.merge(param_hash) { |param, value| eval('"' + value + '"') }
+  end
+  # Create the user resources
+  scope.function_create_resources(['user', users_rsrc_hash])
+
+  # Manage the home directory from the filtered user resources hash
+  users_rsrc_hash.each do |title, param_hash|
     if param_hash.has_key?('home') then
       # JJM I'm not sure if it's best to call
       # the create_resources() function or declare the resource
