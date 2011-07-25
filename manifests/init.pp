8c7b5bbf28c1f9be125a1412e17dafa7cd2476a7
Jeff McCune <jeff@puppetlabs.com>
2011-07-25 14:17:46 -0700

    (#8575) Add YAML data file support
    This change gives the end user the ability to specify the following
    three data structures as a simple YAML file:
     * users_hash
     * users_hash_default
     * groups_hash
    
    This is in contrast to only being able to define accounts and groups
    using a Puppet hash variable.
    
    The data must be stored in Puppet $confdir/data/ using the following
    three file names:
     * accounts_groups_hash.yaml
     * accounts_users_default_hash.yaml
     * accounts_users_hash.yaml
    
    This change also makes the "yaml" data store the default for the module.
    The end user may switch back to data being defined in a Puppet namespace
    by using:
    
        class { 'accounts': data_store => namespace }
    
    Which will default to looking for variables in accounts::data
    
    Reviewed-by: Dan Bode

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,6 +25,10 @@
 #  in the $users_hash_default variable.  Please see the accounts::data class
 #  included in this module for an example.
 #
+#  [*data_store*] Where the data specifying accounts and groups live.  This setting
+#  may be 'yaml' or 'namespace'.  When set to namespace the puppet class specified
+#  with the data_namespace class parameter will be used.
+#
 # Actions:
 #
 # Requires:
@@ -38,6 +42,7 @@
 #     notify { 'alpha': }
 #     ->
 #     class  { 'accounts':
+#       data_store => 'yaml',
 #     }
 #     ->
 #     notify { 'omega': }
@@ -46,9 +51,12 @@
 class accounts (
   $manage_groups  = true,
   $manage_users   = true,
+  $data_store     = 'yaml',
   $data_namespace = 'accounts::data'
 ) {
 
+  validate_re($data_store, '^namespace$|^yaml$')
+  $data_store_real = $data_store
   validate_re($data_namespace, '::data$')
   $data_namespace_real = $data_namespace
   validate_bool($manage_groups)
@@ -56,17 +64,54 @@ class accounts (
   validate_bool($manage_users)
   $manage_users_real = $manage_users
 
-  # Make sure to evaluate the data namespace before trying to pull
-  # data from it.
-  include "${data_namespace_real}"
+  case $data_store_real {
+    namespace: {
+      # Make sure the namespace is added to the catalog.
+      include "${data_namespace_real}"
+
+      # This section of the code is repsonsible for pulling in the data we need.
+      if $manage_groups_real {
+        $groups_hash = getvar("${data_namespace_real}::groups_hash")
+        validate_hash($groups_hash)
+      }
+      if $manage_users_real {
+        # This section of the code is repsonsible for pulling in the data we need.
+        $users_hash = getvar("${data_namespace_real}::users_hash")
+        validate_hash($users_hash)
+        # The default hash will be merged into the users hash.
+        $users_hash_default = getvar("${data_namespace_real}::users_hash_default")
+        validate_hash($users_hash_default)
+      }
+    }
+
+    yaml: {
+      # Figure out where the default puppet confdir is for the master.
+      $datadir = inline_template('<%= File.join(Puppet[:confdir], "data") %>')
+      # Load the hash data from YAML
+      if $manage_users_real {
+        # The files the end user defines data in.
+        $users_hash_file = inline_template("<%= File.join('${datadir}', 'accounts_users_hash.yaml')%>")
+        $users_hash_default_file = inline_template("<%= File.join('${datadir}', 'accounts_users_default_hash.yaml')%>")
+        # Load the files and validate the basic data types.
+        $users_hash = loadyaml($users_hash_file)
+        validate_hash($users_hash)
+        $users_hash_default = loadyaml($users_hash_default_file)
+        validate_hash($users_hash_default)
+      }
+      if $manage_groups_real {
+        # The files the end user defines data in.
+        $groups_hash_file = inline_template("<%= File.join('${datadir}', 'accounts_groups_hash.yaml')%>")
+        # Load the files and validate the basic data types.
+        $groups_hash = loadyaml($groups_hash_file)
+        validate_hash($groups_hash)
+      }
+    }
+  }
 
   anchor { "accounts::begin": }
   anchor { "accounts::end": }
 
   if $manage_groups_real {
-    # This section of the code is repsonsible for pulling in the data we need.
-    $groups_hash = getvar("${data_namespace_real}::groups_hash")
-    validate_hash($groups_hash)
 
     class { 'accounts::groups':
       groups_hash => $groups_hash,
@@ -78,13 +123,6 @@ class accounts (
   }
 
   if $manage_users_real {
-    # This section of the code is repsonsible for pulling in the data we need.
-    $users_hash = getvar("${data_namespace_real}::users_hash")
-    validate_hash($users_hash)
-    # The default hash will be merged into the users hash.
-    $users_hash_default = getvar("${data_namespace_real}::users_hash_default")
-    validate_hash($users_hash_default)
-
     # FIXME We're relying on $users_hash being in scope
     # when this class is declared.
     class { 'accounts::users': }
