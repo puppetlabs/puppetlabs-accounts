f503eb52fb57b5720ca24ef84d08b6542253524b
Jeff McCune <jeff@puppetlabs.com>
2011-08-02 18:32:15 -0700

    (#8678) Refactor accounts::user removing hash use
    Before this patch the accounts::user defined resource type required a
    Hash data structure to be passed to the user_params parameter.  This
    provided a clean interface to create_resources() but is not at all
    friendly to the end user of Puppet.
    
    In order to meet the goal of reducing the new user learning on-ramp and
    to support the use case of resource defaults, this change removes the
    requirement of the user_params hash and replaces the user-facing
    interface with a thin proxy to all of the User resource types we're
    managing.
    
    Previous behavior:
    
        accounts::user { 'jeff':
          user_params => {
            ensure => present,
            uid    => 501,
            gid    => 501,
          }
          locked => false,
        }
    
    New behavior with the patch:
    
        # Support Resource Defaults
        Accounts::User { shell => '/bin/zsh' }
        # Hide the complexity of create_resources()
        accounts::user { 'jeff':
          ensure => present,
          uid    => 501,
          gid    => 501,
          locked => false,
        }
    
    Reviewed-by: Dan Bode

--- manifests/user.pp
+++ manifests/user.pp
@@ -13,20 +13,86 @@
 # declared in the catalog before accounts::user resources are declared.
 #
 define accounts::user(
-  $user_params = {},
-  $locked = false,
-  $sshkeys = [],
-  $data_namespace = 'accounts'
+  $ensure     = 'present',
+  $shell      = '/bin/bash',
+  $comment    = 'UNSET',
+  $home       = 'UNSET',
+  $uid        = 'UNSET',
+  $gid        = 'UNSET',
+  $groups     = [ ],
+  $membership = 'minimum',
+  $password   = '!!',
+  $locked     = false,
+  $sshkeys    = []
 ) {
+  # Validate our inputs from the end user using a "best effort" strategy
+  # ensure
+  validate_re($ensure, '^present$|^absent$')
+  $ensure_real = $ensure
+  # shell
+  validate_re($shell, '^/')
+  $shell_real = $shell
+  # comment
+  if "$comment" == 'UNSET' {
+    $comment_real = $name
+  } else {
+    # we need validate_string() here
+    # But, we allow the end user to specify comment => undef.
+    $comment_real = $comment
+  }
+  # home
+  if "$home" == 'UNSET' {
+    $home_real = "/home/${name}"
+  } else {
+    validate_re($home, '^/')
+    $home_real = $home
+  }
+  # uid number
+  if "$uid" == 'UNSET' {
+    $uid_real = undef
+  } else {
+    validate_re($uid, '^\d+$')
+    $uid_real = $uid
+  }
+  # gid number
+  if "$gid" == 'UNSET' {
+    $gid_real = undef
+  } else {
+    validate_re($gid, '^\d+$')
+    $gid_real = $gid
+  }
+  # groups
+  # FIXME: Validate we've been given an Array
+  $groups_real = $groups
+  # membership
+  validate_re($membership, '^inclusive$|^minimum$')
+  $membership_real = $membership
+  # password
+  # FIXME: Not sure how to validate this input...  It could be most anything?
+  $password_real = $password
+  # locked
+  validate_bool($locked)
+  $locked_real = $locked
+  # sshkeys
+  # FIXME: Not sure how to valiate this input...  Array of keys?
+  $sshkeys_real = $sshkeys
 
-  # NOTE: The data_namespace should be declared in the catalog before
-  # accounts::user resources are declared.
-  $users_hash_default = getvar("${data_namespace}::users_hash_default")
-  validate_hash($users_hash_default)
+  # Build the Hash to feed to create_resources()
+  $user_params = {
+    ensure     => $ensure_real,
+    shell      => $shell_real,
+    comment    => $comment_real,
+    home       => $home_real,
+    uid        => $uid_real,
+    gid        => $gid_real ? { undef => $name, default => $gid_real },
+    groups     => $groups_real,
+    membership => $membership_real,
+    password   => $password_real,
+  }
 
   # if the account should be locked, create a hash to be
   # merged over the user_params hash.
-  if $locked {
+  if $locked_real {
     case $::operatingsystem {
       'debian', 'ubuntu' : {
         $locked_shell = '/usr/sbin/nologin'
@@ -43,17 +109,9 @@ define accounts::user(
     $shell_param = {}
   }
 
-  # if a home directory was not provided, assume a reasonable
-  # default
-  if has_key($user_params, 'home') {
-    $home_param = {}
-  } else {
-    $home_param = {'home' => "/home/${name}"}
-  }
-
-  # merge the user_params over the user defaults, then merge over the
-  # shell and home hashes that we just created.
-  $user_params_real = merge($users_hash_default, $user_params, $shell_param, $home_param)
+  # Replace the shell attribute with the locked shell if
+  # the hash declared above is not empty.
+  $user_params_real = merge($user_params, $shell_param)
 
   # create our user
   $user_hash = {"${name}" => $user_params_real}
@@ -61,23 +119,17 @@ define accounts::user(
 
   # create the primary group
   # what if gid is not a number?
-  if has_key($user_params, 'gid') {
-    group { $name:
-      gid    => $user_params_real['gid'],
-      ensure => $user_params_real['ensure'],
-      before => "User[${name}]",
-    }
+  group { $name:
+    ensure => $ensure_real,
+    gid    => $gid_real,
+    before => "User[${name}]",
   }
-  # manage home directory if specified
-  if has_key($user_params_real, 'home') {
-    accounts::home_dir { $user_params_real['home']:
-      user     => $name,
-      ssh_keys => $sshkeys,
-    }
-  } else {
-    if $sshkeys != [] {
-      fail("Cannot specify sshkeys without a home directory")
-    }
+
+  # Manage the home directory
+  accounts::home_dir { $user_params_real['home']:
+    user     => $name,
+    ssh_keys => $sshkeys,
+    require  => [ User[$name], Group[$name] ],
   }
 
 }
