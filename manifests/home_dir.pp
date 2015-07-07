3d6d634d593b100171dc6d8d0f0822ae7bae931c
Morgan Haskel <morgan@puppetlabs.com>
2015-07-06 17:19:42 -0700

    Get rid of resource_defaults in home_dir

--- manifests/home_dir.pp
+++ manifests/home_dir.pp
@@ -7,32 +7,54 @@
 # directory
 define accounts::home_dir(
   $user,
-  $sshkeys = []
+  $sshkeys = [],
 ) {
 
   # manage ssh public keys if they were specified
   $key_file = "${name}/.ssh/authorized_keys"
 
-  File { owner => $user, group => $user, mode => '0644' }
-
-  file { [$name, "${name}/.ssh", "${name}/.vim"]:
+  file { $name:
     ensure => directory,
-    mode => '0700',
+    owner  => $user,
+    group  => $user,
+    mode   => '0700',
+  }
+
+  file { "${name}/.ssh":
+    ensure => directory,
+    owner  => $user,
+    group  => $user,
+    mode   => '0700',
+  }
+
+  file { "${name}/.vim":
+    ensure => directory,
+    owner  => $user,
+    group  => $user,
+    mode   => '0700',
   }
 
   # Basic customization (#8582)
   # Bash configuration
   file { "${name}/.bashrc":
-    source  => "puppet:///modules/accounts/shell/bashrc",
+    source => "puppet:///modules/accounts/shell/bashrc",
+    owner  => $user,
+    group  => $user,
+    mode   => '0644',
   }
   file { "${name}/.bash_profile":
-    source  => "puppet:///modules/accounts/shell/bash_profile",
+    source => "puppet:///modules/accounts/shell/bash_profile",
+    owner  => $user,
+    group  => $user,
+    mode   => '0644',
   }
 
   # Manage the file permissions of the authorized_keys file
   file { $key_file:
-   ensure => file,
-   mode   => '0600',
+    ensure => file,
+    owner  => $user,
+    group  => $user,
+    mode   => '0600',
   }
 
   if $sshkeys != [] {
