8be5f9cb653bde565978bb32497d32506a51ef30
Eric Putnam <putnam.eric@gmail.com>
2016-11-22 13:39:30 -0800

    Revert "Allow duplicate keys to be associated with multiple accounts."

--- manifests/manage_keys.pp
+++ manifests/manage_keys.pp
@@ -1,11 +1,10 @@
 #
 define accounts::manage_keys(
   $user,
-  $sshkey,
   $key_file,
 ) {
 
-  $key_array   = split($sshkey, ' ')
+  $key_array   = split($name, ' ')
   $key_type    = $key_array[0]
   $key_content = $key_array[1]
   $key_name    = $key_array[2]
@@ -14,6 +13,7 @@ define accounts::manage_keys(
   ssh_authorized_key { $key_title:
     ensure => present,
     user   => $user,
+    name   => $key_name,
     key    => $key_content,
     type   => $key_type,
     target => $key_file,
