5bdd6f79c8c471f2d6f655c42bffaf2c62805737
Will Meek <william.meek@puppet.com>
2017-10-30 17:00:26 +0000

    Cleanup ruby code via rubocop
    This commit will add code cleanup as corrected
    and alerted by rubocop. It also adds rules to
    execute rubocop on Travis.

--- lib/puppet/functions/accounts_ssh_options_parser.rb
+++ lib/puppet/functions/accounts_ssh_options_parser.rb
@@ -3,18 +3,17 @@
 # pattern should be a string.
 
 Puppet::Functions.create_function(:accounts_ssh_options_parser) do
-  dispatch :accounts_ssh_options_parser_String do
+  dispatch :accounts_ssh_options_parser_string do
     param 'String', :str
   end
 
-  def accounts_ssh_options_parser_String(str)
+  def accounts_ssh_options_parser_string(str)
     words = []
     field = ''
-    str.scan(/\G\,*(?>([^\,\\\'\"]+)|'([^\']*)'|("(?:[^\"\\]|\\.)*")|(\\.?)|(\S))(\,|\z)?/m) do
-      |word, sq, dq, esc, garbage, sep|
+    str.scan(%r{\G\,*(?>([^\,\\\'\"]+)|'([^\']*)'|("(?:[^\"\\]|\\.)*")|(\\.?)|(\S))(\,|\z)?}m) do |word, sq, dq, esc, garbage, sep|
       raise ArgumentError, "Unmatched double quote: #{str.inspect}" if garbage
-      field << (word || sq || dq || esc.gsub(/\\(.)/, '\\1'))
-      field.gsub(/\\=/,'\\=\"')
+      field << (word || sq || dq || esc.gsub(%r{\\(.)}, '\\1'))
+      field.gsub(%r{\\=}, '\\=\"')
       if sep
         words << field
         field = ''
@@ -22,5 +21,4 @@ Puppet::Functions.create_function(:accounts_ssh_options_parser) do
     end
     words
   end
-
 end
