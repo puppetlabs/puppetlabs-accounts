5ab0d3ccf5b71b51ebc0a42b41321de7047a2cac
Drew Leske <drew.leske@computecanada.ca>
2018-08-14 05:22:29 +0000

    Parse ssh key spec with RE instead of space

--- manifests/manage_keys.pp
+++ manifests/manage_keys.pp
@@ -1,31 +1,33 @@
 #
 define accounts::manage_keys(
+  $keyspec,
   $user,
   $key_file,
 ) {
 
-  $key_array   = split($name, ' ')
-  # If the key array doesn't start with ssh or ecdsa, then key_array[0] is
-  # assumed to contain ssh options separated by commas.
-  if $key_array[0] =~ /^ssh|^ecdsa-sha2/ {
-    $key_options = undef
-    $key_type    = $key_array[0]
-    $key_content = $key_array[1]
-    $key_name    = $key_array[2]
-  } else {
-    $key_options = accounts_ssh_options_parser($key_array[0])
-    $key_type    = $key_array[1]
-    $key_content = $key_array[2]
-    $key_name    = $key_array[3]
+  $key_def = $keyspec.match(/^((.*)\s+)?((ssh|ecdsa-sha2).*)\s+(.*)\s+(.*)$/)
+  if (! $key_def) {
+    err("Could not interpret SSH key definition: '$keyspec'")
   }
-  $key_title = "${user}_${key_type}_${key_name}"
+  else {
+    if ($key_def[2]) {
+      $key_options = accounts_ssh_options_parser($key_def[2])
+    } else {
+      $key_options = undef
+    }
+    $key_type    = $key_def[3]
+    $key_content = $key_def[5]
+    $key_name    = $key_def[6]
 
-  ssh_authorized_key { $key_title:
-    ensure  => present,
-    user    => $user,
-    key     => $key_content,
-    type    => $key_type,
-    options => $key_options,
-    target  => $key_file,
+    $key_title = "${user}_${key_type}_${key_name}"
+
+    ssh_authorized_key { $key_title:
+      ensure  => present,
+      user    => $user,
+      key     => $key_content,
+      type    => $key_type,
+      options => $key_options,
+      target  => $key_file,
+    }
   }
 }
