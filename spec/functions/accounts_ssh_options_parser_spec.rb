ce6c14c832bafff6e95f0cf09c4a4079d4138935
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-02-27 14:17:46 +0000

    Add unit test for accounts_ssh_options_parser

---
+++ spec/functions/accounts_ssh_options_parser_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'accounts_ssh_options_parser' do
+  it { is_expected.not_to eq(nil) }
+  it { is_expected.to run.with_params('').and_return([]) }
+  it { is_expected.to run.with_params('"').and_raise_error(ArgumentError, %r{Unmatched double quote}) }
+  it { is_expected.to run.with_params('tunnel="0"').and_return(['tunnel="0"']) }
+  it { is_expected.to run.with_params('restrict,tunnel="0",command="sh /etc/netstart tun0"').and_return(['restrict', 'tunnel="0"', 'command="sh /etc/netstart tun0"']) }
+end
