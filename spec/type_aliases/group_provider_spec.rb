9d04217d563b91871467e935e617dce541276ba1
Robert Vincent <robert.vincent@conning.com>
2019-04-19 04:13:51 +0000

    Add tests for the new type aliases.

---
+++ spec/type_aliases/group_provider_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'Accounts::Group::Provider' do
+  describe 'Valid group provider values' do
+    [
+      'aix',
+      'directoryservice',
+      'groupadd',
+      'ldap',
+      'pw',
+      'windows_adsi',
+    ].each do |value|
+      describe value.inspect do
+        it { is_expected.to allow_value(value) }
+      end
+    end
+  end
+
+  describe 'Invalid group provider values' do
+    [
+      0,
+      false,
+      'GroupAdd',
+      '',
+      {},
+      [],
+      nil,
+    ].each do |value|
+      describe value.inspect do
+        it { is_expected.not_to allow_value(value) }
+      end
+    end
+  end
+end
---
+++ spec/type_aliases/group_resource_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'Accounts::Group::Resource' do
+  describe 'Valid group resource values' do
+    [
+      {},
+      { 'ensure'                   => 'absent'                     },
+      { 'allowdupe'                => false                        },
+      { 'auth_membership'          => true                         },
+      { 'forcelocal'               => false                        },
+      { 'gid'                      => 999                          },
+      { 'members'                  => ['root']                     },
+      { 'name'                     => 'root'                       },
+      { 'provider'                 => 'pw'                         },
+      { 'system'                   => false                        },
+    ].each do |value|
+      describe value.inspect do
+        it { is_expected.to allow_value(value) }
+      end
+    end
+  end
+
+  describe 'Invalid group resource values' do
+    [
+      { 'bogus'                    => true                         },
+      { 'ensure'                   => false                        },
+      { 'allowdupe'                => 'soitenly!'                  },
+      { 'auth_membership'          => 'no'                         },
+      { 'forcelocal'               => -1                           },
+      { 'gid'                      => 'nine'                       },
+      { 'members'                  => [-1]                         },
+      { 'name'                     => '$invalid'                   },
+      { 'provider'                 => 'Custom'                     },
+      { 'system'                   => 'Linux'                      },
+    ].each do |value|
+      describe value.inspect do
+        it { is_expected.not_to allow_value(value) }
+      end
+    end
+  end
+end
