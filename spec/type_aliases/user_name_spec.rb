11008e8e27335850375e71969ecc19bf51a19b0e
Timo Bergemann <extern.timo.bergemann@4com.de>
2021-03-10 15:12:34 +0100

    corrected regular expression for usernames

--- spec/type_aliases/user_name_spec.rb
+++ spec/type_aliases/user_name_spec.rb
@@ -20,7 +20,6 @@ describe 'Accounts::User::Name' do
 
   describe 'Invalid user name values' do
     [
-      '1-bad-dude',                           # Cannot begin with a digit.
       '.hidden',                              # Cannot begin with a period.
       '$money',                               # Cannot begin with a dollar-sign.
       '-kilroy_was_here-',                    # Cannot begin with a dash.
